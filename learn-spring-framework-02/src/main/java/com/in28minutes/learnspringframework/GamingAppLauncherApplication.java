b820e14657d8635a470b636ebb9c64d4d75d3ac2
West <36251578+weezle1@users.noreply.github.com>
2023-03-08 21:45:31 -0500

    Initial push of spring framework 02

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/GamingAppLauncherApplication.java
@@ -0,0 +1,28 @@
+package com.in28minutes.learnspringframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GamingConsole;
+/*import com.in28minutes.learnspringframework.game.MarioGame;
+import com.in28minutes.learnspringframework.game.PacmanGame;
+import com.in28minutes.learnspringframework.game.SuperContraGame;*/
+
+@Configuration
+@ComponentScan("com.in28minutes.learnspringframework.game")
+public class GamingAppLauncherApplication {
+
+	public static void main(String[] args) {
+		
+		try(AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)){
+			
+			context.getBean(GamingConsole.class).up();
+			
+			context.getBean(GameRunner.class).run();
+		}
+	}
+}
---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,24 @@
+package com.in28minutes.learnspringframework.game;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PacmanGame implements GamingConsole {
+
+	public void up() {
+		System.out.println("Move up");
+	}
+
+	public void down() {
+		System.out.println("Move Down");
+	}
+
+	public void left() {
+		System.out.println("Move Left");
+	}
+
+	public void right() {
+		System.out.println("Move Right");
+	}
+	
+}

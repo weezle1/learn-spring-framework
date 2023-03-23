8ebfaf41f4a82cfa36e87ea7f8be1cc68d83ef56
West <36251578+weezle1@users.noreply.github.com>
2023-03-22 23:51:51 -0400

    Re-did BusinessCalculationService with course (both ways work)

---
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learnspringframework/examples/c1/RealWorldContextLauncherApplication.java
@@ -0,0 +1,26 @@
+package com.in28minutes.learnspringframework.examples.c1;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+//import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan
+public class RealWorldContextLauncherApplication {
+
+	public static void main(String[] args) {
+		
+		try(AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(RealWorldContextLauncherApplication.class)){
+			
+			Arrays.stream(context.getBeanDefinitionNames())
+				.forEach(System.out::println);
+			
+			System.out.println(context.getBean(BusinessCalculationService.class).findMax());
+		}
+	}
+}

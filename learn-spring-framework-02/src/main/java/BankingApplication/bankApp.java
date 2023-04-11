3d00653abf2e40c789770a48cccd16e22798dfb4
West <36251578+weezle1@users.noreply.github.com>
2023-04-11 00:33:53 -0400

    Created BankingApplication package

---
+++ learn-spring-framework-02/src/main/java/BankingApplication/bankApp.java
@@ -0,0 +1,26 @@
+package BankingApplication;
+
+/***
+ * 
+ * @author Westl
+ *
+ *https://bootcamp.cvn.columbia.edu/blog/java-projects-for-beginners-to-gain-skills/
+ *
+5. Simple Banking Application
+Creating a basic banking application is a great way to practice your Java skills, as having some grasp of the language 
+will be needed to complete the project.
+
+This project will introduce you to many basic Java concepts like printing, variables, if/else statements, strings, 
+loops, and more. You’ll create an application that allows a user to check a bank balance, deposit money, withdraw money, 
+and exit the interface. Check out this ATM/simple banking app to help you work through this project.
+ *
+ */
+
+public class bankApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
